e41bef6124fda10edd019c9ab47c13d8900125a9
landart <correo@jorgealbaladejo.com>
2015-02-08 19:37:07 +0100

    Database changed to SQLite

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -10,16 +10,10 @@
  * control, so do not include passwords or other sensitive information in this
  * file.
  */
-
 return array(
     'db' => array(
-        'driver'         => 'Pdo',
-        'dsn'            => 'mysql:dbname=restapi;host=localhost',
-        'driver_options' => array(
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-        ),
-        /*'driver' => 'Pdo_Sqlite',
-        'database' => '/../data/db/development.db'*/
+        'driver' => 'Pdo_Sqlite',
+        'database' => __DIR__ . '/../../data/db/restapi.db'
     ),
     
     'service_manager' => array(
--- data/restapi.sql
+++ data/restapi.sql
@@ -1,15 +1,9 @@
-CREATE SCHEMA `restapi`
-
-USE `restapi`;
-
---
--- Table structure for table `customer`
---
-CREATE TABLE `customer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstName` varchar(50) NOT NULL,
-  `lastName` varchar(50) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `email` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+# For SQLite
+CREATE TABLE "customer" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT NOT NULL,
+    "address" TEXT,
+    "email" TEXT NOT NULL
 );
+CREATE TABLE sqlite_sequence(name,seq);
